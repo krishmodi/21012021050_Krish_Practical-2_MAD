a27ce444d29e709e8e80c34db6ae6f0504932c07
krish <krishpmodi@gmail.com>
2023-09-26 12:15:23 +0530

    Initial commit

---
+++ app/src/main/java/com/krishmodi/k50_mad_p2_logcat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.krishmodi.k50_mad_p2_logcat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
